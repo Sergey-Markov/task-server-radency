d3a3f9befebeaef7b29e03eddcb419ef159c0360
Sergey-Markov <1nativefl@gmail.com>
2022-01-25 21:24:39 +0200

    light change in this files

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const routes = require("./routes");
 
