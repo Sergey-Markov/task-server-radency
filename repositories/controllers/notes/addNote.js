38e3bb4eda74759a7c8b583f72201ad2a7efd6db
Sergey-Markov <1nativefl@gmail.com>
2022-01-25 21:23:06 +0200

    created POST addNote with mongoose

--- repositories/controllers/notes/addNote.js
+++ repositories/controllers/notes/addNote.js
@@ -1,8 +1,21 @@
-const notesOperations = require("../../../models/");
+const { Note } = require("../../../models/dbModels");
+const datesFromText = require("../../../services/datesFromText");
+const createNewDate = require("../../../services/createNewDate");
 
 const addNote = async (req, res) => {
-  const { name, category, content } = req.body;
-  const note = await notesOperations.createNote(name, category, content);
+  const { name = "unnamed", category = "Task", content = "" } = req.body;
+
+  const date = createNewDate();
+  const allDatesFromText = datesFromText(content);
+
+  const note = await Note.create({
+    name,
+    category,
+    content,
+    date,
+    allDatesFromText,
+    archived: false,
+  });
   res.status(201).json({
     status: "succes",
     code: 201,
--- services/validationSchems/validationSchems.js
+++ services/validationSchems/validationSchems.js
@@ -2,13 +2,11 @@ const Joi = require("joi");
 
 const joiSchemaForPost = Joi.object({
   name: Joi.string(),
-  category: Joi.symbol()
-    .map({
-      "Random Tought": Symbol("Random Tought"),
-      Task: Symbol("Task"),
-      Idea: Symbol("Idea"),
-    })
-    .required(),
+  category: Joi.symbol().map({
+    "Random Tought": Symbol("Random Tought"),
+    Task: Symbol("Task"),
+    Idea: Symbol("Idea"),
+  }),
   content: Joi.string(),
 });
 
