17aecc2f9d30170c5a2570e754fb511881eaaa98
Sergey-Markov <1nativefl@gmail.com>
2022-02-01 10:06:15 +0200

    изменил код на удаление с 201 на 200

--- repositories/controllers/notes/deleteById.js
+++ repositories/controllers/notes/deleteById.js
@@ -3,9 +3,9 @@ const { Note } = require("../../../models/dbModels");
 const deleteById = async (req, res) => {
   const { id } = req.params;
   const note = await Note.findByIdAndDelete(id);
-  res.status(201).json({
+  res.status(200).json({
     status: "succes",
-    code: 201,
+    code: 200,
     data: note,
   });
 };
